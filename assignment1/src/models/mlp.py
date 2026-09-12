e3e0b9aeabd0e5b2b9036476eb090534f8f14066
Diamond-Hoang <cuongson05@gmail.com>
2026-09-12 21:00:34 +0700

    fix: update MLP architecture

--- assignment1/src/models/mlp.py
+++ assignment1/src/models/mlp.py
@@ -1,16 +1,25 @@
 import torch
 import torch.nn as nn
+from typing import List
 
 class MLPClassifier(nn.Module):
-    def __init__(self, input_dim: int = 28 * 28, hidden_dim: int = 256, num_classes: int = 10, dropout: float = 0.2):
+    def __init__(
+        self,
+        input_dim: int = 28 * 28,
+        hidden_dims: List[int] = [256, 128],
+        num_classes: int = 10,
+        dropout: float = 0.2,
+    ):
         super().__init__()
 
         self.flatten = nn.Flatten()
         self.classifier = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
+            nn.Linear(input_dim, hidden_dims[0]),
+            nn.ReLU(),
+            nn.Linear(hidden_dims[0], hidden_dims[1]),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, num_classes)
+            nn.Linear(hidden_dims[1], num_classes),
         )
 
     def forward(self, X):
